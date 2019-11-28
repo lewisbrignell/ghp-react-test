03ec188afd446ad2556dbc247782c7883a429b93
Lewis <lewisbrignell@gmail.com>
2019-11-28 10:30:24 -0500

    first change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h2>Github Pages React Test</h2>
         </a>
       </header>
     </div>
